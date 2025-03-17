19c9480c926d65f384fe7bb0997e211cda5c873c
DedDushnila <mishashuludyakov@gmail.com>
2025-03-17 23:07:52 +0300

    mod_212

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,19 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat one = (Cat) applicationContext.getBean("cat");
+        one.setName("Biba");
+        System.out.println(one.getName());
+
+        Cat two = (Cat) applicationContext.getBean("cat");
+        two.setName("Boba");
+        System.out.println(two.getName());
+
+        System.out.println("пары бинов для Cat равны: " + (one == two));
+        System.out.println("пары бинов для HelloWorld равны: " + (bean == bean2));
     }
 }
